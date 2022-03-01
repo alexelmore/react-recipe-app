bdb74688611f64e24368757e09855261d7732d28
Alex <aelmore@merkleinc.com>
2022-03-01 10:16:17 -0600

    Setup for RecipeList Component

--- recipe-app/src/pages/home/Home.jsx
+++ recipe-app/src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
 import { useFetch } from "../../hooks/useFetch";
+// ReceipeList Component
+import RecipeList from "../../components/RecipeList";
 // Styles
 import "./Home.css";
 
@@ -10,7 +12,7 @@ function Home() {
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
