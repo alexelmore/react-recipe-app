49266f897591b0916ea7d0355e91dcc2fd9a472c
Alex <aelmore@merkleinc.com>
2022-03-02 07:57:42 -0600

    Setup for display results on Search Component

--- recipe-app/src/pages/search/Search.jsx
+++ recipe-app/src/pages/search/Search.jsx
@@ -1,7 +1,34 @@
+import { useFetch } from "../../hooks/useFetch";
+import { useLocation } from "react-router-dom";
+import RecipeList from "../../components/RecipeList";
 // Styles
 import "./Search.css";
+
 function Search() {
-  return <div>Search</div>;
+  // Init and retrieve queryString using the useLocation Hook
+  const queryString = useLocation().search;
+
+  // Init new queryParams object passing our queryString const as an argument into the JavaScript URLSearchParams API
+  const queryParams = new URLSearchParams(queryString);
+
+  // Get query params using "q" as a term
+  const query = queryParams.get("q");
+
+  // URL containing endpoint for our search
+  const url = "http://localhost:3000/recipes?q=" + query;
+
+  // Setup for sending a fetch request for search term
+  const { data, isPending, error } = useFetch(url);
+
+  // Send request
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="page-title">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
 
 export default Search;
